9306e0b0092a1b3874c4dbc1909d00d57c450ea8
mehul_mac <m.mehul1985@gmail.com>
2022-05-29 01:31:18 -0700

    Adding JavaScript Notes and questions practised

---
+++ index.js
@@ -0,0 +1,59 @@
+<h1>JavaScript Notes</h1>
+
+//1.ways to print in JavaScript
+//console.log("Hello World");
+//alert("me");
+//document.write("this is a document write");
+
+//2.JavaScript console api
+
+// console.log("Hello world", 5 + 5, "Another log");
+// console.warn("this is a warning");
+// console.error("this is an error");
+
+//3.JavaScript Variables
+// Containers to store Data values.
+
+var number1 = 34;
+var number2 = 66;
+// console.log(number1 + number2);
+
+// 4. Datatypes in JavaScript
+
+// Numbers
+var num1 = 555;
+var num2 = 77.88;
+
+// String
+var str1 = "This is a String";
+var str2 = 'This is also a String';
+
+//Objects (Key-value pairs)
+var marks = {
+    ravi : 35,
+    rahul : 66,
+    mehul : 99.955
+ }
+// console.log(num1);
+// console.log(num2);
+// console.log(str1);
+// console.log(str2);
+// console.log(marks);
+
+//booleans
+var a = true;
+var b = false;
+// console.log(a , b);
+
+//undefined
+var aa = undefined;
+var bb;
+// console.log(aa , bb);
+
+//null
+var cc = null;
+// console.log(cc);
+
+//arrays
+var dd = [1,2,3,4,5,"mike"]
+// console.log(dd[5]);
